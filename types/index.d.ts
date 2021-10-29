0c1c2ede3c1ec26387c9c2a3bad9531d27ca5c2c
TimurLavrentsov <timur@softrize.com>
2021-10-29 17:20:07 +0300

    feat: add types for lib loader
    feat: update version of node-sass

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,9 +7,19 @@ export interface VueGoogleMapOptions {
   libraries: Array<string>
 }
 
+export interface LibLoader {
+  ensureReady: () => Promise<any>;
+  readyPromises: any[];
+  loaded: boolean;
+  load: () => Promise<void>;
+  _setLoaded: () => void;
+}
+
 export default class VueGoogleMap {
   constructor(options: VueGoogleMapOptions);
 
   static install(): PluginFunction<any>;
+
+  static libLoader: LibLoader;
 }
 
